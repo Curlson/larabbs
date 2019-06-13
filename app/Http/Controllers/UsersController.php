d45026dab6dcb50c4886bb507a021d6c435ef959
Curlson <18556368687@163.com>
2019-06-13 08:22:01 +0000

    feat: upload avatar of users

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Handlers\ImageUploadHandler;
 use App\Http\Requests\UserUpdateRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -19,9 +20,17 @@ class UsersController extends Controller
         return view('users.edit', compact('user'));
     }
     
-    public function update(UserUpdateRequest $request, User $user)
+    public function update(UserUpdateRequest $request,ImageUploadHandler $upload, User $user)
     {
-        $user->update($request->all());
+        $data = $request->all();
+        
+        if($request->avatar) {
+            $result = $upload->save($request->avatar, 'avatars', $user->id);
+            if($result) {
+                $data['avatar'] = $result['path'];
+            }
+        }
+        $user->update($data);
         return redirect()->route('users.show', $user->id)->with('success', '个人资料修改成功!');
     }
     
