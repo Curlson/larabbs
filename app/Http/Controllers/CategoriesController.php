819f98d94ece5265acf630daed44eb94ac6c49cf
Curlson <18556368687@163.com>
2019-06-14 09:39:58 +0000

    feat: topics sort outputting

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -8,9 +8,9 @@ use App\Models\Topic;
 
 class CategoriesController extends Controller
 {
-	public function show( Category $category)
+	public function show(Request $request, Category $category)
 	{
-		$topics = Topic::with('user', 'category')->where('category_id', $category->id)->paginate(20);
+		$topics = Topic::withOrder($request->order)->where('category_id', $category->id)->paginate(20);
 
 		return view('topics.index', compact('topics', 'category'));
 		
--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -32,4 +32,34 @@ class Topic extends Model
     {
         return $this->belongsTo(User::class);
     }
+    
+    public function scopeWithOrder($query, $order)
+    {
+        // 不通的排序, 使用不通的数据读取逻辑
+        switch ($order) {
+            case 'recent':
+                $query->recent();
+                break;
+                
+            default:
+                $query->recentReplied();
+                break;
+        }
+        
+        // 预加载防止 N+1 问题
+        return $query->with('user', 'category');
+    }
+    
+    public function scopeRecent($query)
+    {
+        // 按照创建时间排序
+        return $query->orderBy('created_at', 'desc');
+    }
+    
+    public function scopeRecentReplied($query)
+    {
+        // 当话题有心得回复时, 我们将重新编写逻辑更新话题模型 reply_cout
+        // 此时会自动触发框架对数据模型 updated_at 时间戳更新
+        return $query->orderBy('updated_at', 'desc');
+    }
 }
