862f39abb0b285df43369f543b67523dd61b19d7
sunrui <18556368687@163.com>
2019-06-18 16:27:28 +0800

    feat: only owner can delete the topic

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,9 @@ class User extends Authenticatable implements MustVerifyEmailContract
     {
         return $this->hasMany(Topic::class);
     }
+    
+    public function isAuthorOf($model)
+    {
+        return \Auth::id() == $model->user_id;
+    }
 }
