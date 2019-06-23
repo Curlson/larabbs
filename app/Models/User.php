2b6031e24cb3e0a864ed46995e5978c0dfa402c7
curlson <814801076@qq.com>
2019-06-23 15:09:52 +0000

    feat: 用户管理后台

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,32 +6,33 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Auth\MustVerifyEmail as  MustVerifyEmailTrait;
 use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Support\Str;
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable implements MustVerifyEmailContract
 {
     use MustVerifyEmailTrait;
     use HasRoles;
-    
+
     use Notifiable {
         notify as protected laravelNotify;
     }
-    
+
     public function notify($instance)
     {
         // 如果要通知的人事当前用户, 就不比通知了!
-        if($this->id == \Auth::id()) {
-            return ;
+        if ($this->id == \Auth::id()) {
+            return;
         }
-        
+
         // 只有数据库类型通知才需要提醒, 直接发送 Email 或者其他的都 Pass
-        if(method_exists($instance, 'toDatabase')) {
+        if (method_exists($instance, 'toDatabase')) {
             $this->increment('notification_count');
         }
-        
+
         $this->laravelNotify($instance);
     }
-    
+
     /**
      * The attributes that are mass assignable.
      *
@@ -58,22 +59,22 @@ class User extends Authenticatable implements MustVerifyEmailContract
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-    
-    public function  topics()
+
+    public function topics()
     {
         return $this->hasMany(Topic::class);
     }
-    
+
     public function replies()
     {
         return $this->hasMany(Reply::class);
     }
-    
+
     public function isAuthorOf($model)
     {
         return \Auth::id() == $model->user_id;
     }
-    
+
     /**
      * 将通知数量置位 0
      */
@@ -82,5 +83,33 @@ class User extends Authenticatable implements MustVerifyEmailContract
         $this->notification_count = 0;
         $this->save();
         $this->unreadNotifications->markAsRead();
+    }
+
+    /**
+     * 用户密码修改器
+     * @param $value
+     */
+    public function setPasswordAttribute($value)
+    {
+        // 如果长度等于 60，即认为是已经做过加密的情况
+        if (strlen($value) != 60) {
+
+            // 不等于 60 ， 做密码加密处理
+            $value = bcrypt($value);
+        }
+
+        $this->attributes['password'] = $value;
+    }
+
+    public function setAvatarAttribute($path)
+    {
+        // 如果不是 `http` 字串开头，那就是从后台上传的，需要补全 URL
+        if( ! Str::startsWith($path, 'http')) {
+
+            // 拼接完整的 URL
+            $path = config('app.url') . "/uploads/images/avatars/{$path}";
+        }
+
+        $this->attributes['avatar'] = $path;
     }
 }
