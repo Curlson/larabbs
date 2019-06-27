b473a20efc6464018d736f0268eab071072ea924
sunrui <18556368687@163.com>
2019-06-27 16:50:27 +0800

    feat: 注册用户默认头像

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -11,9 +11,11 @@ class UserObserver
 {
     public function creating(User $user)
     {
-        //
+        if(empty($user->avatar)) {
+            $user->avatar = 'https://cdn.learnku.com/uploads/images/201710/30/1/TrJS40Ey5k.png';
+        }
     }
-
+    
     public function updating(User $user)
     {
         //
