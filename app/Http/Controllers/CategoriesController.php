8246ab783c8ec65e1ccd589e92afb467a8e748f7
sunrui <18556368687@163.com>
2019-06-27 10:16:12 +0800

    feat: 侧边栏链接

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -2,17 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Link;
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Models\Category;
 use App\Models\Topic;
 
 class CategoriesController extends Controller
 {
-	public function show(Request $request, Category $category)
+	public function show(Request $request, Category $category, Topic $topic, User $user, Link $link)
 	{
-		$topics = Topic::withOrder($request->order)->where('category_id', $category->id)->paginate(20);
+		$topics = $topic::withOrder($request->order)->where('category_id', $category->id)->paginate(20);
+		
+		// 资源链接
+		$links = $link->getAllCached();
+		
+		// 活跃用户
+		$active_users = $user->getActiveUsers();
 
-		return view('topics.index', compact('topics', 'category'));
+		return view('topics.index', compact('topics', 'category', 'links', 'active_users'));
 		
 	}
 }
